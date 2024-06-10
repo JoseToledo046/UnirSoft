08753d8f7b8875962e7ac27a83625d678f89c4b1
JoseToledo046 <163978703+JoseToledo046@users.noreply.github.com>
2024-06-10 09:19:43 -0500

    Delete BackEnd directory

--- BackEnd/CargarDatos.md
+++
@@ -1,8 +0,0 @@
-Instalar Python
-
-https://www.python.org/downloads/
-
-Instalar dependencias:
-
-python -m pip install requests
-python -m pip install rmysql-connector-python
--- BackEnd/Spring/operador/src/main/java/com/Operador/operador/Repositorio/RespositorioOperador.java
+++
@@ -1,9 +0,0 @@
-package com.Operador.operador.Repositorio;
-
-import com.Operador.operador.Entidad.Operacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RespositorioOperador extends JpaRepository<Operacion, Long> {
-}
