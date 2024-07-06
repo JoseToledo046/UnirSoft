16299e5511b45ef3406e549cc0d341263144b7e9
JoseToledo046 <joseroberto.toledo046@comunidadunir.net>
2024-07-06 15:06:10 -0500

    Carrusel Luis

--- unirstream/src/components/CarruselList.jsx
+++ unirstream/src/components/CarruselList.jsx
@@ -1,24 +1,49 @@
-import { useRef, useState } from "react";
-import { Carrusel } from "../components/Carrusel";
-import { useCarrusel } from "../hooks/useCarrusel";
+import {useRef, useState,useEffect} from "react";
+import '../styles/Carousel.css';
+/*import {Carrusel} from "../components/Carrusel";
+import {useCarrusel} from "../hooks/useCarrusel";*/
 
 
-export const CarruselList = () => {
+export const CarruselList = ({images, interval = 5000}) => {
 
-    const {carrusel} = useCarrusel();
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentImageIndex((prevIndex) =>
+                prevIndex === images.length - 1 ? 0 : prevIndex + 1
+            );
+        }, interval);
+
+        return () => clearInterval(intervalId);
+    }, [images, interval]);
+
+    const nextSlide = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        );
+    };
+
+    const prevSlide = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === 0 ? images.length - 1 : prevIndex - 1
+        );
+    };
 
     return (
-
-        <div className="carrusel">
-            {carrusel && carrusel.map((movie, index) => (
-                <Carrusel
-                key={index}
-                id = {movie.idFilm}
-                dirImagen = {movie.backdropPath}
-                />
-            ))}
+        <div className="carousel">
+            <button className="carousel__button" onClick={prevSlide}>
+                Anterior
+            </button>
+            <img
+                className="carousel__image"
+                src={images[currentImageIndex]}
+                alt={`Slide ${currentImageIndex + 1}`}
+            />
+            <button className="carousel__button" onClick={nextSlide}>
+                Siguiente
+            </button>
         </div>
-
-    )
+    );
 
 }
--- unirstream/src/views/Home.jsx
+++ unirstream/src/views/Home.jsx
@@ -5,11 +5,16 @@ import { MovieList } from "../components/MovieList"
 import {CarruselList} from "../components/CarruselList";
 
 function Home() {
+    const images = [
+        'https://static.posters.cz/image/hp/75998.jpg',
+        'https://static.posters.cz/image/hp/106363.jpg',
+        'https://static.posters.cz/image/hp/65856.jpg',
+    ];
 
     return (
         <div>
             <NavBar />
-            <CarruselList />
+            <CarruselList images={images}/>
             <MovieList />
             <Footer />
         </div>
