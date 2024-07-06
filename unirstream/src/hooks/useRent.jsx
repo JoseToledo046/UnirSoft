6b0ab3cee8446d7df59726ef630cd29c366452fc
carpiordaniel <44626955+carpiordaniel@users.noreply.github.com>
2024-07-06 14:57:59 -0500

    update

--- unirstream/src/hooks/useRent.jsx
+++ unirstream/src/hooks/useRent.jsx
@@ -1,18 +1,62 @@
 import { useState } from 'react';
 
 export const useRent = () => {
+    console.log("jgsjdh")
+
     const [filmRent, setFilmRent] = useState(() => {
         const storedRent = sessionStorage.getItem('filmRent');
         return storedRent ? JSON.parse(storedRent) : [];
     });
 
+
+    const handleConfirmCancel = (id, original_title, poster_path) => {
+        if (window.confirm("¿Estás seguro de que deseas proceder?")) {
+            const peliculaRenta = {
+                "userId": "JTOLEDO",
+                "movieIds": [parseInt(id)], // [id],
+                "type": "R",
+                "traFecfin": "2025-07-07"
+            }         
+            alert("Has confirmado la acción.");    
+            console.log(peliculaRenta)
+            fetch('https://unirsoft-gateway-production.up.railway.app/operador-service/api/transacciones', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(peliculaRenta),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                    alert("Se ha rentado la película con exito");
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+
+        } else {
+          alert("Has cancelado la acción.");
+        }
+      };
+
     const addRent = (id, original_title, poster_path) => {
-        const updatedRent = [...filmRent, { id, original_title, poster_path }];
-        setFilmRent(updatedRent);
-        sessionStorage.setItem('filmRent', JSON.stringify(updatedRent));
+        // console.log(id, original_title, poster_path)
+        handleConfirmCancel(id, original_title, poster_path);
+        // const updatedRent = [...filmRent, { id, original_title, poster_path }];
+        // setFilmRent(updatedRent);
+        // console.log(updatedRent)
+        //sessionStorage.setItem('filmRent', JSON.stringify(updatedRent));
     };
 
     return { filmRent, addRent };
 };
 
 export default useRent;
+
+// {
+//     "userId": "JTOLEDO",
+//     "movieIds": [19],
+//     "type": "R",
+//     "traFecfin": "2024-07-07"
+// }
--- unirstream/src/hooks/useShop.jsx
+++ unirstream/src/hooks/useShop.jsx
@@ -6,10 +6,45 @@ export const useShop = () => {
         return storedShop ? JSON.parse(storedShop) : [];
     });
 
+    const handleConfirmCancel = (id, original_title, poster_path) => {
+        if (window.confirm("¿Estás seguro de que deseas proceder?")) {
+            const peliculaRenta = {
+                "userId": "JTOLEDO",
+                "movieIds": [parseInt(id)], // [id],
+                "type": "V",
+                "traFecfin": "2025-07-07"
+            }         
+            alert("Has confirmado la acción.");    
+            console.log(peliculaRenta)
+            fetch('https://unirsoft-gateway-production.up.railway.app/operador-service/api/transacciones', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(peliculaRenta),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                    alert("Se ha comprado la película con exito");
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+
+        } else {
+          alert("Has cancelado la acción.");
+        }
+      };
+
+
     const addShop = (id, original_title, poster_path) => {
-        const updatedShop = [...filmShop, { id, original_title, poster_path }];
-        setfilmShop(updatedShop);
-        sessionStorage.setItem('filmShop', JSON.stringify(updatedShop));
+        // console.log(id)
+        handleConfirmCancel(id, original_title, poster_path);
+
+        // const updatedShop = [...filmShop, { id, original_title, poster_path }];
+        // setfilmShop(updatedShop);
+        // sessionStorage.setItem('filmShop', JSON.stringify(updatedShop));
     };
 
     return { filmShop, addShop };
